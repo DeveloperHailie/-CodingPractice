e5453b6442dfa8a5cbedcbe6d3a260898a452ff4
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2022-05-14 02:26:41 +0900

    [update] vector sort 후 비교, map 이용

--- C++/applications/programmers_42576.cpp
+++ C++/applications/programmers_42576.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 // 완주하지 못한 선수
 // https://programmers.co.kr/learn/courses/30/lessons/42576
+
+// vector sort후 비교
 string solution(vector<string> participant, vector<string> completion) {
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
@@ -17,5 +19,36 @@ string solution(vector<string> participant, vector<string> completion) {
     }
     
     string answer = participant[i];
+    return answer;
+}
+
+// map을 이용. 
+string solution2(vector<string> participant, vector<string> completion) {
+    map<string, int> participantMap; // 참가자map (이름,명수)
+    string participantName;
+    for(int i=0;i<participant.size();i++){
+        participantName = participant[i];
+        if(participantMap.find(participant[i])==participantMap.end()){
+            participantMap[participantName] = 1;
+        }else{
+            participantMap[participantName] = 
+                participantMap[participantName]+1;
+        }
+    }
+    
+    // 참가자map에서 completion한 사람 한명씩 제거
+    for(int i=0;i<completion.size();i++){
+        participantName = completion[i];
+        int aParticipantNumber =
+            participantMap.find(participantName)->second;
+        if(aParticipantNumber==1){
+            participantMap.erase(participantName);
+        }else if(aParticipantNumber>1){
+            participantMap[participantName] = 
+                aParticipantNumber-1;
+        }
+    }
+    
+    string answer = participantMap.begin()->first;
     return answer;
 }
